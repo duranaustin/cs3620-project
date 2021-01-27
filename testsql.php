a1026c555465f4a625d6e1e4990095a9a985a8fd
Austin Duran <austin.duran39@gmail.com>
2021-01-26 18:31:46 -0700

    latest sql commit

---
+++ testsql.php
@@ -0,0 +1,24 @@
+<?php
+$servername = "cs3620-sql.mysql.database.azure.com";
+$username = "aduran@cs3620-sql";
+$password = "9456123aAa!";
+$dbname = "cs3620";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+  die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "INSERT INTO books (idBook, bookTitle, bookAuthor)
+VALUES (2,'Extreme Ownership', 'Some Seal')";
+
+if ($conn->query($sql) === TRUE) {
+  echo "New record created successfully";
+} else {
+  echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+?> 
