201c48ccaeb8ae91ff6cacaf895106fd4b09b1d9
Austin Duran <austin.duran39@gmail.com>
2021-02-13 13:59:51 -0700

    comment out to test

--- users.php
+++ users.php
@@ -9,9 +9,9 @@
     $username = $_GET["username"];
     $first_name = $_GET["first_name"];
     $last_name = $_GET["last_name"];
-    $id = $_GET["id"];
+    //$id = $_GET["id"];
     $user = new user();
-    $user->getUser($id);
+    //$user->getUser($id);
     $user=>getUserByData($username, $first_name, $last_name);
 
 
