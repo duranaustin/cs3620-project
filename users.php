6ca3abfa1e3ae07f4c44d5b85eea7f74cccedeb3
Austin Duran <austin.duran39@gmail.com>
2021-02-13 12:49:12 -0700

    edits to hopefully work

--- users.php
+++ users.php
@@ -6,13 +6,13 @@
     error_reporting(E_ALL);
 
    require_once('./user/user.php');
-
+    $username = $_GET["username"];
+    $first_name = $_GET["first_name"];
+    $last_name = $_GET["last_name"];
+    $id = $_GET["id"];
     $user = new user();
-    echo ("get id is equal to " $_GET("id"))
-    if($_GET["id"] != null){
-        $user->getUser($_GET["id"]);
-    }
-    $user=>getUserByData($_GET["username"], $GET["first_name"], $GET["last_name"]);
+    $user->getUser($id);
+    $user=>getUserByData($username, $first_name, $last_name);
 
 
     echo json_encode($user);
