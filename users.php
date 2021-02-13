36b0b39ac8efdf0ca348554819ea3bf2ec84495f
Austin Duran <austin.duran39@gmail.com>
2021-02-13 15:57:10 -0700

    isset

--- users.php
+++ users.php
@@ -7,7 +7,7 @@
 
     require_once('./user/user.php');
     $user = new user();
-    if($_GET["id"] != null){
+    if(if isset($_GET["id"])){
         $user->getUser($_GET["id"]);
     }else{
         $user->getUserByData($_GET["username"], $_GET["first_name"], $_GET["last_name"]);
