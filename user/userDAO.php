42fc56e2617144d7bc1fc68f4b8b0bd2a708cc7e
Austin Duran <austin.duran39@gmail.com>
2021-02-13 11:14:28 -0700

    userDAO update

--- user/userDAO.php
+++ user/userDAO.php
@@ -1,4 +1,9 @@
 <?php
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
 class UserDAO {
   function getUser($user){
     require_once('./utilities/connection.php');
