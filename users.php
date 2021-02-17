ff23e7e9683f62ab87ebb873238220d40f21ff67
Austin Duran <austin.duran39@gmail.com>
2021-02-16 17:40:45 -0700

    another possible fix to users.php

--- users.php
+++ users.php
@@ -12,14 +12,19 @@
     if(isset($_GET["id"])){
         $user->getUser($_GET["id"]);
     }
-    if(isset($_GET["username"])){
+    elseif(isset($_GET["username"])){
          $user->getUserByUsername($_GET["username"]);
     }
-    if(isset($_GET("firstname"))){
+    elseif(isset($_GET("firstname"))){
          $user->getUserByFirstName($_GET["firstname"]);
     }
-    if(isset($_GET["lastname"])){
+    elseif(isset($_GET["lastname"])){
          $user->getUserByLastName($_GET["lastname"]);
+    }
+    else{
+        echo '<script language="javascript">';
+        echo 'alert("You did not provide data in any of the boxes")';
+        echo '</script>';    
     }
 
     echo json_encode($user);
